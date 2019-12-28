82edc29abda5dca4ef1f27a32418d5aa6116baee
Quang Dong <lequangdongg@gmail.com>
2019-12-28 11:02:45 +0700

    redux-build

---
+++ precache-manifest.62916e4f37856582948953caa2cf016d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f31490dde70fdbab751ec214f03f1b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92f24f27bfeb47c542ed",
+    "url": "/static/css/main.3a1fdc66.chunk.css"
+  },
+  {
+    "revision": "6ae9dde214628c33817a",
+    "url": "/static/js/2.053b9553.chunk.js"
+  },
+  {
+    "revision": "92f24f27bfeb47c542ed",
+    "url": "/static/js/main.3722edfb.chunk.js"
+  },
+  {
+    "revision": "cf6c62aee762a9a773da",
+    "url": "/static/js/runtime-main.031cfd2e.js"
+  }
+]);
